9b400003a3f66f751e247be2341d6805d243a47f
Joao Victor Girard <jvgirard@telemidia.puc-rio.br>
2018-03-23 16:36:58 -0300

    maint: Add gst-transmision tool

---
+++ transmission/gst-transmission.h
@@ -0,0 +1,43 @@
+
+#include <gst/app/gstappsink.h>
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <string>
+#include <stdio.h>
+#include <set>
+
+using namespace std;
+
+class GstSigGen
+{
+public:
+  GstSigGen ();
+  ~GstSigGen ();
+  void start ();
+  void stop ();
+  void setFreq (const string &value);
+  void setVolume (const string &value);
+  struct
+  {                         // audio pipeline
+    GstElement *src;        // Audio Test Src format
+    GstElement *convert;    // convert audio format
+    GstElement *tee;        // splits pipeline
+    GstElement *audioQueue; // links audio pipeline side
+    GstElement *audioSink;  // audio sink
+
+    GstPad *teeAudioPad;   // tee audio pad (output)
+    GstPad *queueAudioPad; // queue audio pad (input)
+  } _audio;
+  GstElement *_pipeline;          // pipeline
+  int _sample_flag;               // true if new sample is available
+  GstAppSinkCallbacks _callbacks; // video app-sink callback data
+
+  struct
+  {
+    double freq;   // frequency
+    double volume; // sound level
+  } _prop;
+
+  // GStreamer callbacks.
+  static gboolean cb_Bus (GstBus *, GstMessage *, GstSigGen *);
+};
