b74cb827009ae7531a0cf3a291b32511d835c17d
Joao Victor Girard <jvgirard@telemidia.puc-rio.br>
2018-07-17 13:48:20 -0300

    Minor update in volume settings

--- transmission/gst-transmission.cpp
+++ transmission/gst-transmission.cpp
@@ -242,51 +242,51 @@ main ()
   player13.start ();
 
   player1.setFreq ("20000");
-  player1.setVolume ("0");
+  player1.setVolume ("0.3");
   player1.start ();
 
   player2.setFreq ("20150");
-  player2.setVolume ("0");
+  player2.setVolume ("0.3");
   player2.start ();
 
   player3.setFreq ("20300");
-  player3.setVolume ("0");
+  player3.setVolume ("0.3");
   player3.start ();
 
   player4.setFreq ("20450");
-  player4.setVolume ("0");
+  player4.setVolume ("0.3");
   player4.start ();
 
   player5.setFreq ("20600");
-  player5.setVolume ("0");
+  player5.setVolume ("0.3");
   player5.start ();
 
   player6.setFreq ("20750");
-  player6.setVolume ("0");
+  player6.setVolume ("0.3");
   player6.start ();
 
   player7.setFreq ("20900");
-  player7.setVolume ("0");
+  player7.setVolume ("0.3");
   player7.start ();
 
   player8.setFreq ("21050");
-  player8.setVolume ("0");
+  player8.setVolume ("0.3");
   player8.start ();
 
   player9.setFreq ("21200");
-  player9.setVolume ("0");
+  player9.setVolume ("0.3");
   player9.start ();
 
   player10.setFreq ("21350");
-  player10.setVolume ("0");
+  player10.setVolume ("0.3");
   player10.start ();
 
   player11.setFreq ("21500");
-  player11.setVolume ("0");
+  player11.setVolume ("0.3");
   player11.start ();
 
   player12.setFreq ("21650");
-  player12.setVolume ("0");
+  player12.setVolume ("0.3");
   player12.start ();
 
   while (true)
@@ -296,7 +296,9 @@ main ()
       auto start = std::chrono::system_clock::now ();
 
       short dataToSend[]
-          = { 7, 7, 19, 7, 7, 7  }; // highest number should be 2^13-1 = 4095                
+          // = { 7, 7, 19, 7, 7, 7  }; // highest number should be 2^13-1 = 4095       
+      = { 1, 17, 33, 49, 65, 81};
+      // = { 1, 1, 1, 1, 1, 1};
           // = { 1, 7, 5, 4, 19, 1 }; // highest number should be 2^13-1 = 4095
           // = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4095 }; // highest number should be 2^13-1 = 4095
 
@@ -320,74 +322,74 @@ main ()
           if (number[0])
             player1.setVolume ("1");
           else
-            player1.setVolume ("0");
+            player1.setVolume ("0.3");
           if (number[1])
             player2.setVolume ("1");
           else
-            player2.setVolume ("0");
+            player2.setVolume ("0.3");
           if (number[2])
             player3.setVolume ("1");
           else
-            player3.setVolume ("0");
+            player3.setVolume ("0.3");
           if (number[3])
             player4.setVolume ("1");
           else
-            player4.setVolume ("0");
+            player4.setVolume ("0.3");
           if (number[4])
             player5.setVolume ("1");
           else
-            player5.setVolume ("0");
+            player5.setVolume ("0.3");
           if (number[5])
             player6.setVolume ("1");
           else
-            player6.setVolume ("0");
+            player6.setVolume ("0.3");
           if (number[6])
             player7.setVolume ("1");
           else
-            player7.setVolume ("0");
+            player7.setVolume ("0.3");
           if (number[7])
             player8.setVolume ("1");
           else
-            player8.setVolume ("0");
+            player8.setVolume ("0.3");
           if (number[8])
             player9.setVolume ("1");
           else
-            player9.setVolume ("0");
+            player9.setVolume ("0.3");
           if (number[9])
             player10.setVolume ("1");
           else
-            player10.setVolume ("0");
+            player10.setVolume ("0.3");
           if (number[10])
             player11.setVolume ("1");
           else
-            player11.setVolume ("0");
+            player11.setVolume ("0.3");
           if (number[11])
             player12.setVolume ("1");
           else
-            player12.setVolume ("0");
+            player12.setVolume ("0.3");
 
-          std::this_thread::sleep_for (std::chrono::milliseconds (1000));
+          std::this_thread::sleep_for (std::chrono::milliseconds (100));
 
           for (int t = 0; t < size; t++)
             {
               number[t] = 0;
             }
 
-          player1.setVolume ("0");
-          player2.setVolume ("0");
-          player3.setVolume ("0");
-          player4.setVolume ("0");
-          player5.setVolume ("0");
-          player6.setVolume ("0");
-          player7.setVolume ("0");
-          player8.setVolume ("0");
-          player9.setVolume ("0");
-          player10.setVolume ("0");
-          player11.setVolume ("0");
-          player12.setVolume ("0");
+          player1.setVolume ("0.3");
+          player2.setVolume ("0.3");
+          player3.setVolume ("0.3");
+          player4.setVolume ("0.3");
+          player5.setVolume ("0.3");
+          player6.setVolume ("0.3");
+          player7.setVolume ("0.3");
+          player8.setVolume ("0.3");
+          player9.setVolume ("0.3");
+          player10.setVolume ("0.3");
+          player11.setVolume ("0.3");
+          player12.setVolume ("0.3");
           
 
-          std::this_thread::sleep_for (std::chrono::milliseconds (150));
+          std::this_thread::sleep_for (std::chrono::milliseconds (300));
         }
 
       auto end = std::chrono::system_clock::now ();
